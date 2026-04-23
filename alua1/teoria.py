bca361dcbbeea13641edb499a5ca05bfc0f52195
professor <professor@SN-407016.edu.fiesc.com.br>
2026-04-22 21:53:49 -0300

    meu primeiro codigo python

---
+++ alua1/teoria.py
@@ -0,0 +1,10 @@
+# ISSO E UM COMENTARIO
+# AGORA,NOS ESCREVERMOS NA TELA USANDO PRINT
+
+# O COMANDO PRINT,SERVE PARA ESCREVERMOS ALGUMA COISA.
+
+print("ola, mundo!")
+print("esse E o cruso de logica de orogramaçao")
+
+# o comando input()serve para recber uma entrada"
+# doteclado,") 
